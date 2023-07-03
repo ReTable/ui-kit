bf2eaf43899da62f2a97b62ca79beab2975ab531
Alexey Plutalov <demiazz.py@gmail.com>
2023-07-03 17:32:32 +0300

    build: add stories search in the components and hooks

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,36 @@
+import { readdirSync } from 'node:fs';
+import { dirname, join, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { capitalCase } from 'change-case';
+
+const ROOT_DIR = resolve(dirname(fileURLToPath(import.meta.url)), '../');
+
+function searchStories(workspace) {
+  try {
+    const workspaceDir = join(ROOT_DIR, workspace);
+
+    const stories = [];
+
+    for (const entry of readdirSync(workspaceDir)) {
+      const packageDir = join(workspaceDir, entry);
+      const packageJson = require(join(packageDir, 'package.json'));
+
+      const [_, name] = packageJson.name.split('/');
+
+      stories.push({
+        directory: packageDir,
+        titlePrefix: `${capitalCase(workspace)}/${name}@${packageJson.version}`,
+        files: 'stories/**/*.stories.@(mdx|ts|tsx)',
+      });
+    }
+
+    return stories;
+  } catch {
+    return [];
+  }
+}
+
 export default {
   addons: [
     '@storybook/addon-essentials',
@@ -23,5 +56,5 @@ export default {
 
   staticDirs: ['./public'],
 
-  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [...searchStories('components'), ...searchStories('hooks')],
 };
