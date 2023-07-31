98554dce2402d741e307e18985089f93097194db
Alexey Plutalov <demiazz.py@gmail.com>
2023-07-31 20:13:00 +0400

    feat(ui-icon): add entry points for transformation sizes

--- components/ui-icon/stories/Transformations.stories.tsx
+++ components/ui-icon/stories/Transformations.stories.tsx
@@ -2,7 +2,9 @@ import { FC } from 'react';
 
 import { Meta } from '@storybook/react';
 
-import * as Components from '~/transformations';
+import * as LargeComponents from '~/transformations/large';
+import * as MediumComponents from '~/transformations/medium';
+import * as SmallComponents from '~/transformations/small';
 
 import { container, icon, root } from './styles.css';
 
@@ -32,8 +34,38 @@ export default meta;
 
 // region Stories
 
-export const Icons: FC<Args> = ({ currentColor }) => {
-  const icons = Object.entries(Components).map(([iconName, Icon]) => (
+export const SmallIcons: FC<Args> = ({ currentColor }) => {
+  const icons = Object.entries(SmallComponents).map(([iconName, Icon]) => (
+    <div className={container} key={iconName}>
+      <Icon className={icon} />
+      {iconName}
+    </div>
+  ));
+
+  return (
+    <div className={root} style={{ color: currentColor }}>
+      {icons}
+    </div>
+  );
+};
+
+export const MediumIcons: FC<Args> = ({ currentColor }) => {
+  const icons = Object.entries(MediumComponents).map(([iconName, Icon]) => (
+    <div className={container} key={iconName}>
+      <Icon className={icon} />
+      {iconName}
+    </div>
+  ));
+
+  return (
+    <div className={root} style={{ color: currentColor }}>
+      {icons}
+    </div>
+  );
+};
+
+export const LargeIcons: FC<Args> = ({ currentColor }) => {
+  const icons = Object.entries(LargeComponents).map(([iconName, Icon]) => (
     <div className={container} key={iconName}>
       <Icon className={icon} />
       {iconName}
