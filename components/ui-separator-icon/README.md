8aa58a55806e28dbabe059f016f747e257c9c8da
Alexey Plutalov <demiazz.py@gmail.com>
2023-08-07 17:30:27 +0300

    feat(ui-separator-icon): add `@tabula/ui-separator-icon` package

---
+++ components/ui-separator-icon/README.md
@@ -0,0 +1,27 @@
+# @tabula/ui-separator-icon
+
+Icons for separators.
+
+## Installation
+
+Use the package manager [pnpm](https://pnpm.io) to install `@tabula/ui-separator-icon`.
+
+```bash
+pnpm add @tabula/ui-separator-icon
+```
+
+You can use [npm](https://npmjs.com) or [yarn](https://yarnpkg.com) too.
+
+## Usage
+
+```tsx
+import { UiCommaIcon } from '@tabula/ui-separator-icon';
+
+function ComponentWithIcon() {
+  return <UiCommaIcon />
+}
+```
+
+## License
+
+This project is [ISC](https://choosealicense.com/licenses/isc/) licensed.
---
+++ components/ui-separator-icon/stories/UiSeparatorIcon.stories.tsx
@@ -0,0 +1,58 @@
+import { FC } from 'react';
+
+import { Meta } from '@storybook/react';
+
+import * as Components from '~';
+
+import { container, icon, root } from './styles.css';
+
+// region Args
+
+type Args = {
+  currentColor: string;
+  search: string;
+};
+
+// endregion
+
+// region Meta
+
+const meta: Meta = {
+  title: 'Icons',
+
+  argTypes: {
+    currentColor: {
+      name: 'Color',
+      control: 'color',
+    },
+    search: {
+      name: 'Search',
+      control: 'text',
+    },
+  },
+};
+
+export default meta;
+
+// endregion
+
+// region Stories
+
+export const Icons: FC<Args> = ({ currentColor, search = '' }) => {
+  const icons = Object.entries(Components)
+    .filter(([iconName]) => iconName.includes(search))
+    .map(([iconName, Icon]) => (
+      <div className={container} key={iconName}>
+        <Icon className={icon} />
+        {iconName}
+      </div>
+    ));
+
+  return (
+    <div className={root} style={{ color: currentColor }}>
+      {icons}
+    </div>
+  );
+};
+
+// endregion
