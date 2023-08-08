994527ecbc1705191ba34c3ba3cd744c9cebc738
Alexey Plutalov <demiazz.py@gmail.com>
2023-08-08 18:56:39 +0300

    docs(ui-data-type-icon): fix icons list styles

--- components/ui-data-type-icon/stories/styles.css.ts
+++ components/ui-data-type-icon/stories/styles.css.ts
@@ -10,10 +10,10 @@ export const container = style({
   display: 'flex',
   flexDirection: 'row',
   alignItems: 'center',
-  padding: '16px',
+  padding: '8px',
 });
 
 export const icon = style({
   display: 'block',
-  margin: '16px',
+  marginRight: '16px',
 });
