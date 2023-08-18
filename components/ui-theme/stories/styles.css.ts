ffb31a90c7d26a00e4e4c1487b7d493b021e8bfb
Alexey Plutalov <demiazz.py@gmail.com>
2023-08-18 13:20:02 +0300

    docs(ui-theme): fix stories

--- components/ui-theme/stories/styles.css.ts
+++ components/ui-theme/stories/styles.css.ts
@@ -2,7 +2,7 @@ import { style } from '@vanilla-extract/css';
 
 export const fontsRoot = style({
   display: 'grid',
-  gridTemplateColumns: '1fr 1fr',
+  gridTemplateColumns: '1fr',
   gap: '8px',
 });
 
