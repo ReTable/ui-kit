a5494a3b21b7206a43c9f81a75686aa72d3f43f3
Alexey Plutalov <demiazz.py@gmail.com>
2023-08-30 13:00:37 +0300

    refactor(ui-button): add `wrap` helper

--- components/ui-button/src/UiButton.css.ts
+++ components/ui-button/src/UiButton.css.ts
@@ -1,46 +1,44 @@
 import { style } from '@vanilla-extract/css';
 
-import { uiLayers, uiTheme } from '@tabula/ui-theme';
+import { uiTheme } from '@tabula/ui-theme';
 
 import { disabled } from './marks.css';
 
-export const root = style({
-  '@layer': {
-    [uiLayers.components]: {
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-      background: 'transparent',
-      border: '1px solid transparent',
-      cursor: 'pointer',
-      whiteSpace: 'nowrap',
-      userSelect: 'none',
+import { wrap } from './helpers';
 
-      selectors: {
-        '&:focus': {
-          outlineStyle: 'solid',
-          outlineColor: uiTheme.colors.borderControl.focus2,
-        },
+export const root = style(
+  wrap({
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    background: 'transparent',
+    border: '1px solid transparent',
+    cursor: 'pointer',
+    whiteSpace: 'nowrap',
+    userSelect: 'none',
 
-        [`&${disabled}`]: {
-          background: uiTheme.colors.fillControl.btnDisabled,
-          borderColor: 'transparent',
-          color: uiTheme.colors.content.disabled,
-          boxShadow: 'unset',
-          cursor: 'default',
-          pointerEvents: 'none',
-        },
+    selectors: {
+      '&:focus': {
+        outlineStyle: 'solid',
+        outlineColor: uiTheme.colors.borderControl.focus2,
+      },
+
+      [`&${disabled}`]: {
+        background: uiTheme.colors.fillControl.btnDisabled,
+        borderColor: 'transparent',
+        color: uiTheme.colors.content.disabled,
+        boxShadow: 'unset',
+        cursor: 'default',
+        pointerEvents: 'none',
       },
     },
-  },
-});
+  }),
+);
 
-export const frozen = style({
-  '@layer': {
-    [uiLayers.components]: {
-      cursor: 'default',
-      pointerEvents: 'none',
-    },
-  },
-});
+export const frozen = style(
+  wrap({
+    cursor: 'default',
+    pointerEvents: 'none',
+  }),
+);
--- components/ui-button/src/helpers.ts
+++ components/ui-button/src/helpers.ts
@@ -32,38 +32,42 @@ type VariantStyle = {
 
 // endregion
 
+// region Helpers
+
+export function wrap(styleRule: StyleRule): StyleRule {
+  return {
+    '@layer': {
+      [uiLayers.components]: styleRule,
+    },
+  };
+}
+
+// endregion
+
 // region Root Styles
 
 export function buildRootStyles(rootStyle: RootStyle): [string, string] {
-  const base = style({
-    '@layer': {
-      [uiLayers.components]: {
-        gap: `${rootStyle.base.gap}px`,
-        height: `${rootStyle.base.height}px`,
-        padding: `0 ${rootStyle.base.padding}px`,
-        borderRadius: `${rootStyle.base.borderRadius}px`,
+  const base = style(
+    wrap({
+      gap: `${rootStyle.base.gap}px`,
+      height: `${rootStyle.base.height}px`,
+      padding: `0 ${rootStyle.base.padding}px`,
+      borderRadius: `${rootStyle.base.borderRadius}px`,
 
-        selectors: {
-          '&:focus': {
-            outlineWidth: `${rootStyle.base.outlineWidth}px`,
-          },
+      selectors: {
+        '&:focus': {
+          outlineWidth: `${rootStyle.base.outlineWidth}px`,
         },
       },
-    },
-  });
+    }),
+  );
 
   const icon =
     'paddingLeft' in rootStyle.withIcon
       ? { paddingLeft: `${rootStyle.withIcon.paddingLeft}px` }
       : { paddingRight: `${rootStyle.withIcon.paddingRight}px` };
 
-  const withIcon = style({
-    '@layer': {
-      [uiLayers.components]: {
-        ...icon,
-      },
-    },
-  });
+  const withIcon = style(wrap(icon));
 
   return [base, withIcon];
 }
@@ -91,17 +95,7 @@ export function buildVariant(root: string, variant: VariantStyle): ComplexStyleR
     selectors[`&:not(:disabled, ${disabled}):focus`] = variant.focus;
   }
 
-  return [
-    root,
-    variant.font,
-    {
-      '@layer': {
-        [uiLayers.components]: {
-          selectors,
-        },
-      },
-    },
-  ];
+  return [root, variant.font, wrap({ selectors })];
 }
 
 // endregion
