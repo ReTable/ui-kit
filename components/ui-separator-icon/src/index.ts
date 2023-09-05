242e4c403e758a1ac35c3b24c1d8474faf0c3f47
Alexey Plutalov <demiazz.py@gmail.com>
2023-09-05 11:32:35 +0300

    feat(ui-separator-icon): re-export icon urls along with icon components

--- components/ui-separator-icon/src/index.ts
+++ components/ui-separator-icon/src/index.ts
@@ -1,5 +1,5 @@
-export { ReactComponent as UiCommaIcon } from './comma.svg';
-export { ReactComponent as UiDotIcon } from './dot.svg';
-export { ReactComponent as UiSemicolonIcon } from './semicolon.svg';
-export { ReactComponent as UiSpaceIcon } from './space.svg';
-export { ReactComponent as UiTabIcon } from './tab.svg';
+export { default as uiCommaIconUrl, ReactComponent as UiCommaIcon } from './comma.svg';
+export { default as uiDotIconUrl, ReactComponent as UiDotIcon } from './dot.svg';
+export { default as uiSemicolonIconUrl, ReactComponent as UiSemicolonIcon } from './semicolon.svg';
+export { default as uiSpaceIconUrl, ReactComponent as UiSpaceIcon } from './space.svg';
+export { default as uiTabIconUrl, ReactComponent as UiTabIcon } from './tab.svg';
