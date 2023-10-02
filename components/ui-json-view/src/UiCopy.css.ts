2605b1f62ca0dc043382aa1ccb02ee3900fd0ad9
Alexey Plutalov <demiazz.py@gmail.com>
2023-10-02 18:16:26 +0300

    fix(ui-json-view): show copied status for clipboard

---
+++ components/ui-json-view/src/UiCopy.css.ts
@@ -0,0 +1,7 @@
+import { style } from '@vanilla-extract/css';
+
+import { uiTheme } from '@tabula/ui-theme';
+
+export const copied = style({
+  color: uiTheme.colors.content.success,
+});
--- components/ui-json-view/src/UiCopy.tsx
+++ components/ui-json-view/src/UiCopy.tsx
@@ -1,4 +1,8 @@
-import { FC } from 'react';
+import { FC, MutableRefObject, useRef, useState } from 'react';
+
+import { clsx } from 'clsx';
+
+import { copied } from './UiCopy.css';
 
 import { UiAction } from './UiAction';
 import { ActionFn } from './types';
@@ -8,16 +12,37 @@ type Props = {
   jsonPath: string;
 };
 
-const action: ActionFn = async (jsonPath, query) => {
-  const toCopy = JSON.stringify(query(jsonPath), null, 4);
-
-  await navigator.clipboard.writeText(toCopy);
-};
+const delay = 1000;
 
 export const UiCopy: FC<Props> = ({ className, jsonPath }) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const timerRef: MutableRefObject<number | null> = useRef(null);
+
+  const action: ActionFn = (_, query) => {
+    const toCopy = JSON.stringify(query(jsonPath), null, 4);
+
+    navigator.clipboard
+      .writeText(toCopy)
+      .then(() => {
+        setIsCopied(true);
+
+        if (timerRef.current != null) {
+          window.clearTimeout(timerRef.current);
+        }
+
+        timerRef.current = window.setTimeout(() => {
+          setIsCopied(false);
+        }, delay);
+      })
+      .catch(() => {
+        setIsCopied(false);
+      });
+  };
+
   return (
-    <UiAction className={className} action={action} jsonPath={jsonPath}>
-      Copy
+    <UiAction className={clsx(className, isCopied && copied)} action={action} jsonPath={jsonPath}>
+      {isCopied ? 'Copied!' : 'Copy'}
     </UiAction>
   );
 };
