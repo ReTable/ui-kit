49e65927e9603dc9456a952cce7c20ce37512802
Alexey Plutalov <demiazz.py@gmail.com>
2023-10-02 13:02:32 +0300

    fix(ui-json-view): fix styles of the size and data type labels

--- components/ui-json-view/src/UiSize.css.ts
+++ components/ui-json-view/src/UiSize.css.ts
@@ -3,11 +3,11 @@ import { style } from '@vanilla-extract/css';
 import { uiFonts, uiLayers, uiTheme } from '@tabula/ui-theme';
 
 export const root = style([
-  uiFonts.monospace.regular10,
+  uiFonts.monospace.regular12,
   {
     '@layer': {
       [uiLayers.components]: {
-        color: uiTheme.colors.content.secondary,
+        color: uiTheme.colors.content.placeholder,
         userSelect: 'none',
       },
     },
