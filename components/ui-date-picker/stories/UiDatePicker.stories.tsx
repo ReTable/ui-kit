c9dc2e0bf46b3aef685e5c13cb5e9522cdf8f543
Alexey Plutalov <demiazz.py@gmail.com>
2023-12-20 13:57:05 +0300

    docs(ui-date-picker): update stories

--- components/ui-date-picker/stories/UiDatePicker.stories.tsx
+++ components/ui-date-picker/stories/UiDatePicker.stories.tsx
@@ -1,20 +1,76 @@
-import { FC, useState } from 'react';
+import { FC, useCallback, useState } from 'react';
 
-import { UiDatePicker } from '~';
+import { StoryObj } from '@storybook/react';
+import { format } from 'date-fns';
+
+import { UiDatePicker, UiDatePickerType } from '~';
 
 export default {
   component: UiDatePicker,
-  title: 'ui-date-picker',
+  title: 'UiDatePicker',
 };
 
-export const Default: FC = () => {
+type Props = {
+  type: UiDatePickerType;
+};
+
+const Template: FC<Props> = ({ type }) => {
+  const [id, setId] = useState(0);
   const [selected, setSelected] = useState<Date | null>(null);
+
+  const handleNow = useCallback(() => {
+    setSelected(new Date());
+  }, []);
+
+  const handleReset = useCallback(() => {
+    setSelected(null);
+  }, []);
+
+  const handleUpdate = useCallback(() => {
+    setId((current) => (current += 1));
+  }, []);
 
   return (
     <>
-      <p>{selected?.toLocaleString()}</p>
-
-      <UiDatePicker selected={selected ?? undefined} onSelect={setSelected} type="datetime" />
+      <p style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+        {selected == null ? 'Not selected' : <>{format(selected, 'dd MMM yyyy, HH:mm:ss ')}</>}
+        <button onClick={handleNow} type="button">
+          Set now
+        </button>
+        {selected != null && (
+          <button onClick={handleReset} type="button">
+            Reset
+          </button>
+        )}
+        <button onClick={handleUpdate} type="button">
+          Force update
+        </button>
+      </p>
+      <UiDatePicker key={id} onSelect={setSelected} selected={selected ?? undefined} type={type} />
     </>
   );
+};
+
+export const DateStory: StoryObj = {
+  name: 'Date',
+
+  render() {
+    return <Template type="date" />;
+  },
+};
+
+export const TimeStory: StoryObj = {
+  name: 'Time',
+
+  render() {
+    return <Template type="time" />;
+  },
+};
+
+export const DateTimeStory: StoryObj = {
+  name: 'DateTime',
+
+  render() {
+    return <Template type="datetime" />;
+  },
 };
