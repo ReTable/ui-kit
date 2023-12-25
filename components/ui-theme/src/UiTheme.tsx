1068f8a8a717ae6eb7fc44f4e5de632c7fb32a92
Alexey Plutalov <demiazz.py@gmail.com>
2023-12-25 22:25:10 +0300

    fix(ui-theme): add `displayName` for exported component

--- components/ui-theme/src/UiTheme.tsx
+++ components/ui-theme/src/UiTheme.tsx
@@ -14,4 +14,8 @@ export const UiTheme: FC<PropsWithChildren> = ({ children }) => (
   <Context.Provider value={uiTheme}>{children}</Context.Provider>
 );
 
+if (import.meta.env.DEV) {
+  UiTheme.displayName = 'ui-theme(UiTheme)';
+}
+
 export const useUiTheme = (): typeof uiTheme => useContext(Context);
