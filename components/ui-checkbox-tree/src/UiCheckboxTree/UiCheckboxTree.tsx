aaa40538f5f7a5f081bc011b0da846074ba484bf
Alexey Plutalov <demiazz.py@gmail.com>
2024-04-24 16:15:29 +0300

    docs(ui-checkbox-tree): add docs for properties

--- components/ui-checkbox-tree/src/UiCheckboxTree/UiCheckboxTree.tsx
+++ components/ui-checkbox-tree/src/UiCheckboxTree/UiCheckboxTree.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from 'react';
 
+import { clsx } from 'clsx/lite';
+
 import { Tree, TreeLeaf, UiTree } from '@tabula/ui-tree';
 
 import * as styles from './UiCheckboxTree.css';
@@ -11,30 +13,51 @@ import { Leaf as LeafComponent } from '../Leaf';
 import { ChangeHandler, Selected } from '../types';
 
 export type Props<Leaf extends TreeLeaf> = {
+  /**
+   * User defined CSS class which be assigned to the root element.
+   */
+  className?: string;
+
+  /**
+   * Tree of elements to render.
+   */
   tree: Tree<Leaf>;
 
+  /**
+   * Function which returns a label for the given node.
+   */
   labelOf: LabelGetter<Leaf>;
 
+  /**
+   * Set of selected leaf ids.
+   */
   selected: Selected<Leaf>;
 
+  /**
+   * Handler which called with a new set of selected leaf ids.
+   */
   onChange: ChangeHandler<Leaf>;
 
+  /**
+   * Optional attribute for test purposes.
+   */
   testId?: string;
 };
 
 export function UiCheckboxTree<Leaf extends TreeLeaf>({
-  tree,
+  className,
   labelOf,
-  selected,
   onChange,
+  selected,
   testId,
+  tree,
 }: Props<Leaf>): ReactNode {
   const [rootTestId, headerTestId, treeTestId] =
     testId == null ? [] : [testId, `${testId}--header`, `${testId}--items`];
 
   return (
     <Provider labelOf={labelOf} onChange={onChange} selected={selected} tree={tree}>
-      <div className={styles.root} data-testid={rootTestId}>
+      <div className={clsx(styles.root, className)} data-testid={rootTestId}>
         <Header
           className={styles.header}
           onChange={onChange}
