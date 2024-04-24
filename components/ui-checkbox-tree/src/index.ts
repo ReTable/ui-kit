cd4fb201a440b446e86da0b218bee343d9a2cea1
Alexey Plutalov <demiazz.py@gmail.com>
2024-04-24 16:26:05 +0300

    feat(ui-checkbox-tree): re-export tree types

--- components/ui-checkbox-tree/src/index.ts
+++ components/ui-checkbox-tree/src/index.ts
@@ -5,3 +5,5 @@ export type { UiCheckboxTreeProps } from './UiCheckboxTree';
 export type { LabelGetter } from './Context';
 
 export type { ChangeHandler, Selected } from './types';
+
+export type { TreeLeaf, Tree, TreeNode, TreeBranch } from '@tabula/ui-tree';
