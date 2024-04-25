e89a487cda372125b7e9bddef6a1aa7a3dc54667
Alexey Plutalov <demiazz.py@gmail.com>
2024-04-25 14:43:44 +0300

    test: fix alias for `vitest`

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,32 @@
-import { UserConfig, mergeConfig } from 'vite';
+import path from 'node:path';
+import url from 'node:url';
+
+import { Alias, UserConfig, mergeConfig } from 'vite';
 
 import { browser } from '@tabula/vitest-config';
 
+// region Resolver
+
+const rootDir = path.dirname(url.fileURLToPath(import.meta.url));
+
+const alias: Alias = {
+  find: /^~$/,
+  replacement: `../lib/$1`,
+  customResolver(_, importer) {
+    if (importer == null) {
+      return null;
+    }
+
+    const [ns, pkgName] = path.relative(rootDir, importer).split(path.sep);
+
+    return path.join(rootDir, ns, pkgName, 'lib/index.js');
+  },
+};
+
+// endregion Resolver
+
 export default mergeConfig(browser as UserConfig, {
   resolve: {
-    alias: [
-      {
-        find: /~(.*)$/,
-        replacement: `../lib/$1`,
-      },
-    ],
+    alias: [alias],
   },
 });
