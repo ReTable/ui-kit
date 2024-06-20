eeafb307895029662a090a5de84d6e3b36737958
Alexey Plutalov <demiazz.py@gmail.com>
2024-06-20 19:12:32 +0300

    feat(ui-checkbox): toggle `indeterminate` property on each render

--- components/ui-checkbox/src/UiCheckbox.hooks.ts
+++ components/ui-checkbox/src/UiCheckbox.hooks.ts
@@ -1,4 +1,4 @@
-import { ChangeEventHandler, RefObject, useCallback, useEffect, useRef } from 'react';
+import { ChangeEventHandler, RefObject, useCallback, useRef } from 'react';
 
 type Options = {
   isIndeterminate: boolean;
@@ -12,15 +12,18 @@ export function useLifecycle({ isIndeterminate, onChange }: Options): Result {
 
   // NOTE: Sets the `indeterminate` flag directly to the HTML element, because React doesn't support `indeterminate`
   //       property natively.
-  useEffect(() => {
-    const { current: input } = ref;
-
-    if (input == null) {
-      return;
-    }
-
-    input.indeterminate = isIndeterminate;
-  }, [isIndeterminate]);
+  //
+  //       We don't use `useEffect` here, to avoid the following use case:
+  //
+  //         - input initially is indeterminate;
+  //         - user click by element;
+  //         - outer logic keep input is indeterminate;
+  //         - DOM element in same time can be not indeterminate;
+  //         - because of `isIndeterminate` not changed, DOM element don't changed, and `indeterminate` attribute
+  //           not changed.
+  if (ref.current != null) {
+    ref.current.indeterminate = Boolean(isIndeterminate);
+  }
 
   const handleChange: ChangeEventHandler<HTMLInputElement> = useCallback(
     ({ target }) => {
