dbe17c91cae22ba23eded9f4bcd2d4c84ff39d1f
Alexey Plutalov <demiazz.py@gmail.com>
2024-08-13 23:17:31 +0300

    docs(ui-status-badge): add stories

--- components/ui-status-badge/stories/UiStatusBadge.stories.tsx
+++ components/ui-status-badge/stories/UiStatusBadge.stories.tsx
@@ -1,10 +1,87 @@
-import { FC } from 'react';
+import { StoryObj } from '@storybook/react';
 
-import { UiStatusBadge } from '~';
+import { UiStatusBadge, UiStatusBadgeVariant } from '~';
 
-export default {
-  component: UiStatusBadge,
-  title: 'ui-status-badge',
+// region Meta
+
+type Args = {
+  icon: boolean;
+  customIcon: boolean;
+  label: string;
 };
 
-export const Default: FC = () => <UiStatusBadge />;
+const meta = {
+  title: 'UiStatusBadge',
+
+  component: UiStatusBadge,
+
+  argTypes: {
+    icon: {
+      control: 'boolean',
+      name: 'Show icon?',
+    },
+
+    customIcon: {
+      control: 'boolean',
+      name: 'Use custom icon?',
+      if: {
+        arg: 'icon',
+        eq: true,
+      },
+    },
+
+    label: {
+      control: 'text',
+      name: 'Label',
+    },
+  },
+
+  args: {
+    icon: true,
+    customIcon: false,
+  },
+
+  parameters: {
+    controls: {
+      include: /(icon|Label)/,
+    },
+  },
+};
+
+export default meta;
+
+// endregion Meta
+
+// region Story Utilities
+
+type Story = StoryObj<Args>;
+
+function createStory(initialLabel: string, variant: UiStatusBadgeVariant): Story {
+  return {
+    args: {
+      label: initialLabel,
+    },
+
+    render({ icon, label }) {
+      return (
+        <UiStatusBadge icon={icon} variant={variant}>
+          {label}
+        </UiStatusBadge>
+      );
+    },
+  };
+}
+
+// region Story Utilities
+
+// region Stories
+
+export const Success = createStory('Success', 'success');
+
+export const Active = createStory('Active', 'active');
+
+export const Error = createStory('Error', 'error');
+
+export const Inactive = createStory('Inactive', 'inactive');
+
+// endregion Stories
