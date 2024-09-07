a6714b6891fb654716a8c748e622704a915e4918
Alexey Plutalov <demiazz.py@gmail.com>
2024-09-07 18:50:06 +0300

    test(use-flag): add tests

--- hooks/use-flag/tests/useFlag.test.ts
+++ hooks/use-flag/tests/useFlag.test.ts
@@ -4,21 +4,95 @@ import { describe, expect, it } from 'vitest';
 import { useFlag } from '~';
 
 describe('useFlag', () => {
-  it('works', () => {
-    const { result } = renderHook(useFlag);
+  describe('default value', () => {
+    it('has true value when given true', () => {
+      const { result } = renderHook(() => useFlag(true));
 
-    expect(result.current.counter).toBe(0);
-
-    act(() => {
-      result.current.increment();
+      expect(result.current[0]).toBe(true);
     });
 
-    expect(result.current.counter).toBe(1);
+    it('has true value when given false', () => {
+      const { result } = renderHook(() => useFlag(false));
 
-    act(() => {
-      result.current.decrement();
+      expect(result.current[0]).toBe(false);
     });
+  });
 
-    expect(result.current.counter).toBe(0);
+  describe('change', () => {
+    it('changes flag to the given state', () => {
+      const { result } = renderHook(() => useFlag(false));
+
+      act(() => {
+        result.current[1].change(true);
+      });
+
+      expect(result.current[0]).toBe(true);
+
+      act(() => {
+        result.current[1].change(false);
+      });
+
+      expect(result.current[0]).toBe(false);
+
+      act(() => {
+        result.current[1].change(false);
+      });
+
+      expect(result.current[0]).toBe(false);
+    });
+  });
+
+  describe('toggle', () => {
+    it('toggles flag', () => {
+      const { result } = renderHook(() => useFlag(false));
+
+      act(() => {
+        result.current[1].toggle();
+      });
+
+      expect(result.current[0]).toBe(true);
+
+      act(() => {
+        result.current[1].toggle();
+      });
+
+      expect(result.current[0]).toBe(false);
+    });
+  });
+
+  describe('on', () => {
+    it('enables flag', () => {
+      const { result } = renderHook(() => useFlag(false));
+
+      act(() => {
+        result.current[1].on();
+      });
+
+      expect(result.current[0]).toBe(true);
+
+      act(() => {
+        result.current[1].on();
+      });
+
+      expect(result.current[0]).toBe(true);
+    });
+  });
+
+  describe('off', () => {
+    it('enables flag', () => {
+      const { result } = renderHook(() => useFlag(true));
+
+      act(() => {
+        result.current[1].off();
+      });
+
+      expect(result.current[0]).toBe(false);
+
+      act(() => {
+        result.current[1].off();
+      });
+
+      expect(result.current[0]).toBe(false);
+    });
   });
 });
