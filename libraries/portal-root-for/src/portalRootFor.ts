67fa730733d51651f55148e47cb2d45eaf78c6d8
Alexey Plutalov <demiazz.py@gmail.com>
2024-09-07 20:44:43 +0300

    feat(portal-root-for): add `@tabula/portal-root-for` package

---
+++ libraries/portal-root-for/src/portalRootFor.ts
@@ -0,0 +1,26 @@
+function getRoot(id: string): HTMLDivElement | null {
+  return document.body.querySelector(`#${id}`);
+}
+
+function createRoot(id: string): HTMLDivElement {
+  const root = document.createElement('div');
+
+  root.id = id;
+
+  return document.body.appendChild(root);
+}
+
+type Options = {
+  className?: string;
+  id: string;
+};
+
+export function portalRootFor({ className, id }: Options): HTMLDivElement {
+  const root = getRoot(id) ?? createRoot(id);
+
+  if (className != null) {
+    root.className = className;
+  }
+
+  return root;
+}
