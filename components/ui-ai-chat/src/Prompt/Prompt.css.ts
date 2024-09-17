e9df6401b5d3e0c75ed69622b3497fc585729533
Alexey Plutalov <demiazz.py@gmail.com>
2024-09-17 13:36:33 +0300

    fix(ui-ai-chat): restyle prompt input

---
+++ components/ui-ai-chat/src/Prompt/Prompt.css.ts
@@ -0,0 +1,94 @@
+import { style } from '@vanilla-extract/css';
+
+import { uiLayers, uiStyles, uiTheme } from '@tabula/ui-theme';
+
+export const root = style({
+  '@layer': {
+    [uiLayers.components]: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      gap: '8px',
+      padding: '8px',
+      borderRadius: '16px',
+      backgroundColor: uiTheme.colors.background.controlsDetails,
+    },
+  },
+});
+
+export const isSending = style({});
+
+export const input = style([
+  uiStyles.fonts.sansSerif.medium14,
+  {
+    '@layer': {
+      [uiLayers.components]: {
+        flex: '1 1 auto',
+        border: 'none',
+        background: 'transparent',
+        color: uiTheme.colors.content.primary,
+        resize: 'none',
+
+        selectors: {
+          '&::-webkit-scrollbar': {
+            display: 'none',
+          },
+
+          '&:focus': {
+            outline: 'none',
+          },
+
+          '&::placeholder': {
+            color: uiTheme.colors.content.tertiary,
+          },
+        },
+      },
+    },
+  },
+]);
+
+export const send = style({
+  '@layer': {
+    [uiLayers.components]: {
+      flex: '0 0 auto',
+      alignSelf: 'flex-end',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '32px',
+      height: '32px',
+      border: 'none',
+      borderRadius: '16px',
+      background: uiTheme.colors.fillControl.btnPrimary,
+      color: uiTheme.colors.content.contrast,
+      cursor: 'pointer',
+      transition: `all ${uiTheme.duration.fast['1']} ${uiTheme.easing.standard.productive}`,
+
+      selectors: {
+        '&:focus': {
+          outline: `2px solid ${uiTheme.colors.borderControl.focus2}`,
+          outlineOffset: '0',
+        },
+
+        '&:hover': {
+          background: uiTheme.colors.fillControl.btnPrimaryHover,
+        },
+
+        '&:active': {
+          background: uiTheme.colors.fillControl.btnPrimaryPressed,
+          transition: 'none',
+        },
+
+        '&:disabled': {
+          background: uiTheme.colors.neutral['150'],
+          cursor: 'default',
+        },
+
+        [`${isSending} &:disabled`]: {
+          background: uiTheme.colors.fillControl.btnPrimary,
+        },
+      },
+    },
+  },
+});
