9fe3db50389e619e5fb1980c853dfe56bac7cafb
Alexey Plutalov <demiazz.py@gmail.com>
2024-09-18 21:05:47 +0300

    fix(ui-ai-chat): remove unused consts

--- components/ui-ai-chat/src/shared.css.ts
+++ components/ui-ai-chat/src/shared.css.ts
@@ -1,8 +1,5 @@
 import { styleVariants } from '@vanilla-extract/css';
 
-export const MIN_VISIBLE_ROWS_COUNT = 1;
-export const MAX_VISIBLE_ROWS_COUNT = 10;
-
 export const HEADER_HEIGHT = '32px';
 
 export const variants = styleVariants({
