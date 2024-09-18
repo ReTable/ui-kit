cce957bfb1697e824746fadb678eeb017daf5735
Alexey Plutalov <demiazz.py@gmail.com>
2024-09-18 11:58:48 +0300

    fix(ui-ai-chat): rewrite tables rendering

--- components/ui-ai-chat/src/Answer/Answer.helpers.ts
+++ components/ui-ai-chat/src/Answer/Answer.helpers.ts
@@ -1,108 +1,144 @@
 import DOMPurify from 'dompurify';
 import { xxHash32 } from 'js-xxhash';
-import { Tokens } from 'marked';
+import { Marked, Tokens } from 'marked';
 
 import * as styles from './Answer.css';
 
 import { TableAction, TableData } from '../types';
 
+// region Actions
+
 declare global {
-  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
   interface Window {
-    UI_AI_CHAT_TABLE_ACTIONS_STORAGE: Map<string, () => void>;
+    UI_AI_CHAT_TABLE_ACTIONS: Map<string, () => void>;
   }
 }
 
-window.UI_AI_CHAT_TABLE_ACTIONS_STORAGE =
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-  window.UI_AI_CHAT_TABLE_ACTIONS_STORAGE || new Map<string, () => void>();
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+window.UI_AI_CHAT_TABLE_ACTIONS = window.UI_AI_CHAT_TABLE_ACTIONS || new Map();
 
-function hashTable(table: TableData) {
-  const str = JSON.stringify(table);
-
-  return xxHash32(str);
+function hashOf(table: TableData): string {
+  return xxHash32(JSON.stringify(table)).toString(16);
 }
 
-function createTableActionButton(requestId: number, table: TableData, action: TableAction): string {
-  const key = `${requestId}_${action.label}_${hashTable(table)}`;
+function registerActions(id: number, table: TableData, actions: TableAction[]): string[][] {
+  const prefix = `${id}__${hashOf(table)}`;
 
-  window.UI_AI_CHAT_TABLE_ACTIONS_STORAGE.set(key, () => {
-    action.action(table);
-  });
+  const vtable: string[][] = [];
 
+  for (let idx = 0; idx < actions.length; idx += 1) {
+    const key = `${prefix}__${idx}`;
+    const { label, action } = actions[idx];
+
+    const call = `window.UI_AI_CHAT_TABLE_ACTIONS.get('${key}')?.()`;
+
+    window.UI_AI_CHAT_TABLE_ACTIONS.set(key, () => {
+      action(table);
+    });
+
+    vtable.push([label, call]);
+  }
+
+  return vtable;
+}
+
+export function unregisterActions(id: number): void {
+  for (const [key] of window.UI_AI_CHAT_TABLE_ACTIONS.entries()) {
+    if (!key.startsWith(`${id}__`)) {
+      return;
+    }
+
+    window.UI_AI_CHAT_TABLE_ACTIONS.delete(key);
+  }
+}
+
+function renderActions(id: number, table: TableData, actions: TableAction[]): string {
+  const vtable = registerActions(id, table, actions);
+
+  const items = vtable
+    .map(
+      ([label, call]) => `
+        <button class="${styles.tableActionButton}" onclick="${call}" type="button">${label}</button>
+    `,
+    )
+    .join();
+
+  return `<div class="${styles.tableActions}">${items}</div>`;
+}
+
+// endregion Actions
+
+// region Table
+
+function renderHead(data: string[]): string {
+  const cells = data.map((it) => `<th>${it}</th>`).join('');
+
+  return `<thead><tr>${cells}</tr></thead>`;
+}
+
+function renderBody(data: string[][]): string {
+  const rows = data
+    .map((row) => {
+      const cells = row.map((it) => `<td>${it}</td>`).join('');
+
+      return `<tr>${cells}</tr>`;
+    })
+    .join('');
+
+  return `<tbody>${rows}</tbody>`;
+}
+
+function renderTable(table: TableData): string {
   return `
-<button
-  class="${styles.tableActionButton}"
-  onclick="window.UI_AI_CHAT_TABLE_ACTIONS_STORAGE.get('${key}')()"
->
-  ${action.label}
-</button>`;
-}
-
-export function clearActionsForRequest(requestId: number): void {
-  for (const [key] of window.UI_AI_CHAT_TABLE_ACTIONS_STORAGE.entries()) {
-    if (key.startsWith(`${requestId}_`)) {
-      window.UI_AI_CHAT_TABLE_ACTIONS_STORAGE.delete(key);
-    }
-  }
-}
-
-function renderRow(row: string[]) {
-  return row.map((value) => `<td>${value}</td>`).join('');
-}
-
-function renderRows(rows: string[][]) {
-  return rows.map((row) => `<tr>${renderRow(row)}</tr>`).join('');
-}
-
-function renderHeader(header: string[]) {
-  return `<tr>${header.map((h) => `<th>${h}</th>`).join('')}</tr>`;
-}
-
-export function createTableRenderer(
-  requestId: number,
-  tableActions: TableAction[],
-): (token: Tokens.Generic) => string | false {
-  return (token: Tokens.Generic) => {
-    if (token.type !== 'table') {
-      return false;
-    }
-
-    const { header, rows } = token as Tokens.Table;
-
-    const data: TableData = {
-      header: header.map((h) => h.text),
-      rows: rows.map((row) => row.map((v) => v.text)),
-    };
-
-    const renderedTable = DOMPurify.sanitize(`
+    <div class="${styles.tableScroll}">
       <div class="${styles.tableContainer}">
         <table>
-          <thead>
-            ${renderHeader(data.header)}
-          </thead>
-          <tbody>
-            ${renderRows(data.rows)}
-          </tbody>
+          ${renderHead(table.header)}
+          ${renderBody(table.rows)}
         </table>
       </div>
-    `);
-
-    const actions = tableActions
-      .map((action) => createTableActionButton(requestId, data, action))
-      .join('\n');
-
-    return `
-      <div class="${styles.tableScroll}">${renderedTable}</div>
-      <div class="${styles.tableActions}">${actions}</div>
-    `;
-  };
+    </div>
+  `;
 }
 
-const allowedOnClickContent = /^window\.UI_AI_CHAT_TABLE_ACTIONS_STORAGE\.get\('[\w -]+'\)\(\)$/;
+// endregion Table
+
+// region Render
+
+export function render(id: number, input: string, tableActions: TableAction[]): string {
+  const parser = new Marked({
+    async: false,
+
+    renderer: {
+      table({ header, rows }: Tokens.Table): string {
+        const table: TableData = {
+          header: header.map((h) => h.text),
+          rows: rows.map((row) => row.map((v) => v.text)),
+        };
+
+        console.log(renderTable(table));
+
+        return `${renderTable(table)}${renderActions(id, table, tableActions)}`;
+      },
+    },
+  });
+
+  const output = parser.parse(input, { async: false });
+
+  return DOMPurify.sanitize(output);
+}
+
+// endregion Render
+
+// region DOMPurify
+
+const allowedOnClickContent =
+  /^window\.UI_AI_CHAT_TABLE_ACTIONS\.get\('\d+__[\d\w]+__\d+'\)\?\.\(\)$/;
 
 DOMPurify.addHook('uponSanitizeAttribute', (_, data) => {
   if (data.attrName === 'onclick' && allowedOnClickContent.test(data.attrValue)) {
     data.forceKeepAttr = true;
   }
 });
+
+// endregion DOMPurify
--- components/ui-ai-chat/src/Answer/Answer.tsx
+++ components/ui-ai-chat/src/Answer/Answer.tsx
@@ -1,11 +1,8 @@
-import { ReactNode, useEffect, useState } from 'react';
-
-import DOMPurify from 'dompurify';
-import { marked } from 'marked';
+import { ReactNode, useEffect, useMemo } from 'react';
 
 import { Request, TableAction } from '../types';
 
-import { clearActionsForRequest, createTableRenderer } from './Answer.helpers';
+import { render, unregisterActions } from './Answer.helpers';
 
 type Props = {
   className?: string;
@@ -14,39 +11,32 @@ type Props = {
 };
 
 export function Answer({ className, request, tableActions }: Props): ReactNode {
-  const [sanitizedParsedHtml, setSanitizedParsedHtml] = useState<string>();
-
-  useEffect(() => {
+  const content = useMemo(() => {
     if (request.id == null) {
-      return;
+      return null;
     }
 
-    marked.use({
-      extensions: [{ name: 'table', renderer: createTableRenderer(request.id, tableActions) }],
-    });
+    return render(request.id, request.answer, tableActions);
+  }, [request.id, request.answer, tableActions]);
 
-    // NOTE: The `marked.parse` could return `Promise<string>` if extension requires `async` operation.
-    //
-    //       We use the only one extension, which we controlled, and can be sure it's synchronous.
-    //
-    // FIXME(demiazz): rewrite this piece of code in async style, to avoid type type casting.
-    const parsed = marked.parse(request.answer, { async: false }) as string;
-    const sanitized = DOMPurify.sanitize(parsed);
+  useEffect(
+    () => () => {
+      if (request.id == null) {
+        return;
+      }
 
-    setSanitizedParsedHtml(sanitized);
+      unregisterActions(request.id);
+    },
+    [request?.id],
+  );
 
-    return () => {
-      clearActionsForRequest(request.id);
-    };
-  }, [request, tableActions]);
-
-  if (sanitizedParsedHtml == null) {
+  if (content == null) {
     return <div className={className}>...</div>;
   }
 
   return (
     // eslint-disable-next-line react/no-danger
-    <div className={className} dangerouslySetInnerHTML={{ __html: sanitizedParsedHtml }} />
+    <div className={className} dangerouslySetInnerHTML={{ __html: content }} />
   );
 }
 
