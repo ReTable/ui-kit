d7bd52b917f976b8733a9cab0551d0c05826c255
Alexey Plutalov <demiazz.py@gmail.com>
2024-09-19 17:34:06 +0300

    feat(ui-ai-chat): refactor chat controller

--- components/ui-ai-chat/src/UiAiChat/UiAiChat.hooks.ts
+++ components/ui-ai-chat/src/UiAiChat/UiAiChat.hooks.ts
@@ -1,34 +1,12 @@
-import { Ref, RefObject, useEffect, useImperativeHandle, useRef, useState } from 'react';
+import { Ref, RefObject, useImperativeHandle, useRef } from 'react';
 
 import { Controller } from '../types';
-
-type Direction = 'top' | 'bottom';
 
 export function useController(
   ref: Ref<Controller>,
 ): [RefObject<HTMLDivElement>, RefObject<HTMLTextAreaElement>] {
   const conversationRef = useRef<HTMLDivElement>(null);
   const promptRef = useRef<HTMLTextAreaElement>(null);
-
-  // NOTE: We use state to scroll conversation, because usually we want to
-  //       scroll after some message has been added.
-  //
-  //       If we call `scrollTo` directly on the moment of call, then we
-  //       scroll an element to position before a new elements has been added.
-
-  const [direction, setDirection] = useState<Direction | null>(null);
-
-  useEffect(() => {
-    const { current: conversation } = conversationRef;
-
-    if (conversation == null || direction == null) {
-      return;
-    }
-
-    const y = direction === 'top' ? 0 : conversation.scrollHeight;
-
-    conversation.scrollTo(0, y);
-  });
 
   useImperativeHandle(
     ref,
@@ -37,12 +15,24 @@ export function useController(
         promptRef.current?.focus();
       },
 
-      scrollToTop: () => {
-        setDirection('top');
+      scrollToTop: (behavior?: ScrollBehavior) => {
+        const { current: conversation } = conversationRef;
+
+        if (conversation == null) {
+          return;
+        }
+
+        conversation.scrollTo({ left: 0, top: 0, behavior });
       },
 
-      scrollToBottom: () => {
-        setDirection('bottom');
+      scrollToBottom: (behavior?: ScrollBehavior) => {
+        const { current: conversation } = conversationRef;
+
+        if (conversation == null) {
+          return;
+        }
+
+        conversation.scrollTo({ left: 0, top: conversation.scrollHeight, behavior });
       },
     }),
     [],
