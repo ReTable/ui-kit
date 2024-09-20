66c6f4de72f5bced6623a15073e4df580b786d76
Alexey Plutalov <demiazz.py@gmail.com>
2024-09-20 21:03:21 +0300

    feat(ui-slider)!: reimplement `UiSlider` with native `input[type=range]` element

--- components/ui-slider/src/UiSlider.css.ts
+++ components/ui-slider/src/UiSlider.css.ts
@@ -1,86 +1,169 @@
-import { style, styleVariants } from '@vanilla-extract/css';
+import { CSSProperties } from 'react';
+
+import { createVar, style, styleVariants } from '@vanilla-extract/css';
 
 import { uiLayers, uiTheme } from '@tabula/ui-theme';
 
-export const circleSize = 20;
+// region Constants
+
+const THUMB_SIZE = 20;
+
+// endregion Constants
+
+// region CSS Variables
+
+export const progressVar = createVar();
+
+// endregion CSS Variables
+
+// region Helpers
+
+function trackBackground(from: string, to?: string): string {
+  return [
+    `linear-gradient(to left, ${uiTheme.colors.neutralAlpha['10']} 0%, ${uiTheme.colors.neutralAlpha['10']} calc(100% - ${progressVar}), transparent calc(100% - ${progressVar}), transparent 0)`,
+    `linear-gradient(to left, white 0%, white calc(100% - ${progressVar}), transparent calc(100% - ${progressVar}), transparent 0)`,
+    `linear-gradient(to right, ${from}, ${to ?? from})`,
+  ].join(', ');
+}
+
+// endregion Helpers
+
+// region Track
+
+const track: CSSProperties = {
+  width: '100%',
+  height: '4px',
+  borderRadius: '2px',
+  transition: `background-size ${uiTheme.duration.moderate['1']} ${uiTheme.easing.standard.expressive}`,
+};
+
+const trackDisabled: CSSProperties = {
+  backgroundImage: trackBackground(uiTheme.colors.neutralAlpha['40']),
+};
+
+// endregion Track
+
+// region Thumb
+
+const thumb: CSSProperties = {
+  appearance: 'none',
+  width: `${THUMB_SIZE}px`,
+  height: `${THUMB_SIZE}px`,
+  marginTop: '-8px',
+  border: 'none',
+  borderRadius: '10px',
+  background: uiTheme.colors.background.primaryContent,
+  boxShadow: [
+    `0px 4px 12px 0px ${uiTheme.colors.shadow['16']}`,
+    `0px 1px 2px 0px ${uiTheme.colors.shadow['12']}`,
+  ].join(', '),
+  transition: `background-color ${uiTheme.duration.fast['1']} ${uiTheme.easing.standard.productive}`,
+};
+
+const thumbFocus: CSSProperties = {
+  outline: `2px solid ${uiTheme.colors.borderControl.focus2}`,
+  outlineOffset: '0',
+};
+
+const thumbHover: CSSProperties = {
+  backgroundColor: uiTheme.colors.background.panels,
+};
+
+const thumbActive: CSSProperties = {
+  boxShadow: [
+    `0px 1px 2px 0px ${uiTheme.colors.shadow['16']}`,
+    `0px 1px 2px 0px ${uiTheme.colors.shadow['12']}`,
+  ].join(', '),
+};
+
+const thumbDisabled: CSSProperties = {
+  backgroundColor: uiTheme.colors.neutral['30'],
+  boxShadow: [
+    `0px 2px 6px 0px ${uiTheme.colors.shadow['12']}`,
+    `0px 1px 2px 0px ${uiTheme.colors.shadow['8']}`,
+  ].join(', '),
+  cursor: 'default',
+};
+
+// endregion Thumb
+
+// region Root
 
 export const root = style({
   '@layer': {
     [uiLayers.components]: {
-      position: 'relative',
-      display: 'flex',
-      alignItems: 'center',
-      height: '28px',
+      width: 'auto',
+      height: `${THUMB_SIZE}px`,
+      appearance: 'none',
+      background: 'transparent',
       cursor: 'pointer',
-    },
-  },
-});
-
-export const states = styleVariants({
-  isDragging: {},
-  isMouseDown: {},
-});
-
-export const line = style({
-  '@layer': {
-    [uiLayers.components]: {
-      width: '100%',
-      height: '4px',
-      borderRadius: '2px',
-      background: [
-        uiTheme.colors.neutralAlpha['10'],
-        `linear-gradient(to right, ${uiTheme.colors.fillControl.ai.from}, ${uiTheme.colors.fillControl.ai.to})`,
-        'no-repeat',
-      ].join(' '),
-      transition: `background-size ${uiTheme.duration.moderate['1']} ${uiTheme.easing.standard.expressive}`,
 
       selectors: {
-        [`${states.isDragging} &`]: {
-          transition: 'none',
+        '&:focus': {
+          outline: 'none',
         },
+
+        '&:disabled': {
+          cursor: 'default',
+        },
+
+        // NOTE: The `-webkit-` selectors would be invalid in FF, and vice verse.
+        //
+        //       If one of the selectors is invalid, then all other is invalid too. To prevent this standard behaviour,
+        //       we duplicate styles with dedicated selectors.
+
+        '&::-webkit-slider-runnable-track': track,
+        '&::-moz-range-track': track,
+
+        '&:disabled::-webkit-slider-runnable-track': trackDisabled,
+        '&:disabled::-moz-range-track': trackDisabled,
+
+        '&::-webkit-slider-thumb': thumb,
+        '&::-moz-range-thumb': thumb,
+
+        '&:focus::-webkit-slider-thumb': thumbFocus,
+        '&:focus::-moz-range-thumb': thumbFocus,
+
+        '&::-webkit-slider-thumb:hover': thumbHover,
+        '&::-moz-range-thumb:hover': thumbHover,
+
+        '&::-webkit-slider-thumb:active': thumbActive,
+        '&::-moz-range-thumb:active': thumbActive,
+
+        '&:disabled::-webkit-slider-thumb': thumbDisabled,
+        '&:disabled::-moz-range-thumb': thumbDisabled,
       },
     },
   },
 });
 
-export const circleContainer = style({
-  '@layer': {
-    [uiLayers.components]: {
-      position: 'absolute',
-      top: '4px',
-      transition: `left ${uiTheme.duration.moderate['1']} ${uiTheme.easing.standard.expressive}`,
-
-      selectors: {
-        [`${states.isDragging} &`]: {
-          transition: 'none',
-        },
-      },
+export const variants = styleVariants(
+  {
+    normal: {
+      from: uiTheme.colors.accent['100'],
+      to: uiTheme.colors.accent['100'],
+    },
+    ai: {
+      from: uiTheme.colors.fillControl.ai.from,
+      to: uiTheme.colors.fillControl.ai.to,
     },
   },
-});
+  ({ from, to }) => {
+    const properties: CSSProperties = {
+      backgroundImage: trackBackground(from, to),
+    };
 
-export const circle = style({
-  '@layer': {
-    [uiLayers.components]: {
-      width: `${circleSize}px`,
-      height: `${circleSize}px`,
-      backgroundColor: uiTheme.colors.neutral['0'],
-      borderRadius: '50%',
-      transition: `transform ${uiTheme.duration.moderate['1']}`,
-      boxShadow: [
-        `0 4px 12px ${uiTheme.colors.shadow['16']}`,
-        `0 1px 2px ${uiTheme.colors.shadow['12']}`,
-      ].join(', '),
-
-      selectors: {
-        [`${root}:hover &`]: {
-          transform: 'scale(1.15)',
-        },
-
-        [`${states.isMouseDown} &, ${states.isMouseDown}:hover &`]: {
-          transform: 'scale(0.9)',
+    return {
+      '@layer': {
+        [uiLayers.components]: {
+          selectors: {
+            '&::-webkit-slider-runnable-track': properties,
+            '&::-moz-range-track': properties,
+          },
         },
       },
-    },
+    };
   },
-});
+);
+
+// endregion Root
--- components/ui-slider/src/UiSlider.tsx
+++ components/ui-slider/src/UiSlider.tsx
@@ -1,123 +1,70 @@
-import { MouseEventHandler, ReactNode, useCallback, useEffect, useRef, useState } from 'react';
+import { ChangeEventHandler, ReactNode, useCallback, useMemo } from 'react';
 
+import { assignInlineVars } from '@vanilla-extract/dynamic';
 import { clsx } from 'clsx/lite';
 
 import * as styles from './UiSlider.css';
+import { progressVar } from './UiSlider.css';
 
 export type ChangeHandler = (value: number) => void;
 
+export type Variant = keyof typeof styles.variants;
+
 export type Props = {
-  /**
-   * User defined CSS class which be assigned to the root element.
-   */
   className?: string;
-  onChange: ChangeHandler;
-  /**
-   * The lowest value in the range of permitted values.
-   */
-  min: number;
-  /**
-   * The greatest value in the range of permitted values.
-   */
+  id?: string;
+  isDisabled?: boolean;
   max: number;
-  /**
-   * The input control's value.
-   */
+  min: number;
+  name?: string;
+  onChange: ChangeHandler;
+  step?: number;
   value: number;
+  variant?: Variant;
 };
 
-const INTENTIONAL_DRAG_THRESHOLD = 0.005;
-
-function getStyles(value: number, min: number, max: number) {
-  const percent = ((value - min) / (max - min)) * 100;
-  return {
-    line: {
-      backgroundSize: `${percent}%`,
+export function UiSlider({
+  className,
+  id,
+  isDisabled,
+  max,
+  min,
+  name,
+  onChange,
+  step,
+  value,
+  variant = 'normal',
+}: Props): ReactNode {
+  const handleChange = useCallback<ChangeEventHandler<HTMLInputElement>>(
+    (event) => {
+      onChange(event.target.valueAsNumber);
     },
-    circle: {
-      left: `max(0%, min(100% - ${styles.circleSize}px, calc(${percent}% - ${styles.circleSize / 2}px)))`,
-    },
-  };
-}
-
-function calculateValue(e: MouseEvent, root: HTMLDivElement, min: number, max: number) {
-  const rect = root.getBoundingClientRect();
-  const pos = (e.pageX - rect.x) / rect.width;
-
-  const scale = Math.min(1, Math.max(0, pos));
-
-  return min + scale * (max - min);
-}
-
-export function UiSlider({ className, max, min, onChange, value }: Props): ReactNode {
-  const rootRef = useRef<HTMLDivElement>(null);
-
-  const [dragging, setDragging] = useState(false);
-  const [mouseDown, setMouseDown] = useState(false);
-
-  const { line, circle } = getStyles(value, min, max);
-
-  useEffect(() => {
-    const handleMouseUp = () => {
-      setMouseDown(false);
-      setDragging(false);
-    };
-
-    const handleMouseMove = (e: MouseEvent) => {
-      if (!mouseDown || rootRef.current == null) {
-        return;
-      }
-
-      e.preventDefault();
-
-      const newValue = calculateValue(e, rootRef.current, min, max);
-      const drag = Math.abs(value - newValue) / value;
-
-      if (drag > INTENTIONAL_DRAG_THRESHOLD) {
-        onChange(newValue);
-        setDragging(true);
-      }
-    };
-
-    window.addEventListener('mouseup', handleMouseUp);
-    window.addEventListener('mousemove', handleMouseMove);
-
-    return () => {
-      window.removeEventListener('mouseup', handleMouseUp);
-      window.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, [mouseDown, min, max, onChange, value]);
-
-  const mouseDownHandler = useCallback<MouseEventHandler>(
-    (e) => {
-      if (rootRef.current == null) {
-        return;
-      }
-
-      setMouseDown(true);
-      onChange(calculateValue(e.nativeEvent, rootRef.current, min, max));
-    },
-    [min, max, onChange],
+    [onChange],
   );
 
+  const style = useMemo(() => {
+    // NOTE: Prevent division by zero.
+    const progress = max === 0 ? 0 : Math.min((value / max) * 100, 100);
+
+    return assignInlineVars({
+      [progressVar]: `${progress}%`,
+    });
+  }, [value, max]);
+
   return (
-    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
-    <div
-      ref={rootRef}
-      className={clsx(
-        styles.root,
-        mouseDown && styles.states.isMouseDown,
-        dragging && styles.states.isDragging,
-        className,
-      )}
-      onMouseDown={mouseDownHandler}
-    >
-      <div className={styles.line} style={line}>
-        <div className={styles.circleContainer} style={circle}>
-          <div className={styles.circle} />
-        </div>
-      </div>
-    </div>
+    <input
+      className={clsx(styles.root, styles.variants[variant], className)}
+      disabled={isDisabled}
+      id={id}
+      max={max}
+      min={min}
+      name={name}
+      onChange={handleChange}
+      step={step}
+      style={style}
+      type="range"
+      value={value}
+    />
   );
 }
 
