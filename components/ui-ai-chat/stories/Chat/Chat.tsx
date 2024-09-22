e3608d256101236a294b0d5be5a7acd402d1da88
Alexey Plutalov <demiazz.py@gmail.com>
2024-09-23 00:11:42 +0300

    docs(ui-ai-chat): updated stories

--- components/ui-ai-chat/stories/Chat/Chat.tsx
+++ components/ui-ai-chat/stories/Chat/Chat.tsx
@@ -8,19 +8,31 @@ import { MAX_PROMPT_LENGTH } from '../const';
 import { useChat } from './Chat.hooks';
 
 type Props = {
+  context?: string;
+  onClearContext?: () => void;
+  suggestions?: string[];
   tableActions: TableAction[];
   variant: Variant;
 };
 
-export function Chat({ tableActions, variant }: Props): ReactNode {
+export function Chat({
+  context,
+  onClearContext,
+  suggestions,
+  tableActions,
+  variant,
+}: Props): ReactNode {
   const chat = useChat();
 
   return (
     <Container variant={variant}>
       <UiAiChat
         {...chat}
+        context={context}
         maxPromptLength={MAX_PROMPT_LENGTH}
+        onClearContext={onClearContext}
         placeholder="Ask Universe"
+        suggestions={suggestions}
         tableActions={tableActions}
       />
     </Container>
