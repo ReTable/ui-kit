12f34273c50c750a4fafc7ec2452bdf583f97720
Alexey Plutalov <demiazz.py@gmail.com>
2024-10-22 19:13:55 +0300

    feat(ui-multi-selector): add additional type exports

--- components/ui-multi-selector/src/index.ts
+++ components/ui-multi-selector/src/index.ts
@@ -9,6 +9,9 @@ export type {
   CompleteKey as UiMultiSelectorCompleteKey,
   IconComponent as UiMultiSelectorIconComponentType,
   Option as UiMultiSelectorOption,
+  SelectAll as UiMultiSelectorSelectAll,
+  SelectFound as UiMultiSelectorSelectFound,
+  Selected as UiMultiSelectorSelected,
   Size as UiMultiSelectorSize,
   Variant as UiMultiSelectorVariant,
 } from './types';
