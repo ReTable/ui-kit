3c9064e6695e4cac6bf4bc5f39d96c40403d2a44
amikhaylov-retable <72373692+amikhaylov-retable@users.noreply.github.com>
2024-11-05 21:09:25 +0300

    Feat(ui-node-icon): search node icons (#282)
    * fix: support disabled state on colored S icons
    
    * feat: add icons for search node
    
    * chore: add changeset

--- components/ui-node-icon/src/small.ts
+++ components/ui-node-icon/src/small.ts
@@ -1,3 +1,27 @@
+import { ReactComponent as UiFolderSourceSIconInner } from './folderSource/folderSource.small.svg';
+import { ReactComponent as UiLocalFileSIconInner } from './localFile/localFile.small.svg';
+import { ReactComponent as UiPostgresSourceSIconInner } from './postgresSource/postgresSource.small.svg';
+import { ReactComponent as UiReportSIconInner } from './report/report.small.svg';
+import { ReactComponent as UiSearchCompanySIconInner } from './searchCompany/searchCompany.small.svg';
+import { ReactComponent as UiSearchDeveloperSIconInner } from './searchDeveloper/searchDeveloper.small.svg';
+import { ReactComponent as UiSearchLookalikeCompanySIconInner } from './searchLookalikeCompany/searchLookalikeCompany.small.svg';
+import { ReactComponent as UiSearchPeopleSIconInner } from './searchPeople/searchPeople.small.svg';
+import { ReactComponent as UiSnowflakeSourceSIconInner } from './snowflakeSource/snowflakeSource.small.svg';
+
+import { icon } from './style.css';
+
+import { createIcon } from './utils';
+
+export const UiFolderSourceSIcon = createIcon(UiFolderSourceSIconInner, icon);
+export const UiLocalFileSIcon = createIcon(UiLocalFileSIconInner, icon);
+export const UiPostgresSourceSIcon = createIcon(UiPostgresSourceSIconInner, icon);
+export const UiReportSIcon = createIcon(UiReportSIconInner, icon);
+export const UiSearchCompanySIcon = createIcon(UiSearchCompanySIconInner, icon);
+export const UiSearchDeveloperSIcon = createIcon(UiSearchDeveloperSIconInner, icon);
+export const UiSearchLookalikeCompanySIcon = createIcon(UiSearchLookalikeCompanySIconInner, icon);
+export const UiSearchPeopleSIcon = createIcon(UiSearchPeopleSIconInner, icon);
+export const UiSnowflakeSourceSIcon = createIcon(UiSnowflakeSourceSIconInner, icon);
+
 export { ReactComponent as UiAddGPTColumnSIcon } from './addGPTColumn/addGPTColumn.small.svg';
 export { ReactComponent as UiAddNewColumnSIcon } from './addNewColumn/addNewColumn.small.svg';
 export { ReactComponent as UiApiColumnSIcon } from './apiColumn/apiColumn.small.svg';
@@ -15,25 +39,21 @@ export { ReactComponent as UiEditColumnSIcon } from './editColumn/editColumn.sma
 export { ReactComponent as UiEnrichmentSIcon } from './enrichment/enrichment.small.svg';
 export { ReactComponent as UiExtractSIcon } from './extract/extract.small.svg';
 export { ReactComponent as UiFilterSIcon } from './filter/filter.small.svg';
-export { ReactComponent as UiFolderSourceSIcon } from './folderSource/folderSource.small.svg';
 export { ReactComponent as UiGPTNodeSIcon } from './gptNode/gptNode.small.svg';
 export { ReactComponent as UiGroupBySIcon } from './groupBy/groupBy.small.svg';
 export { ReactComponent as UiInnerJoinSIcon } from './innerJoin/innerJoin.small.svg';
 export { ReactComponent as UiJsonFileSIcon } from './jsonFile/jsonFile.small.svg';
 export { ReactComponent as UiLeftJoinSIcon } from './leftJoin/leftJoin.small.svg';
-export { ReactComponent as UiLocalFileSIcon } from './localFile/localFile.small.svg';
 export { ReactComponent as UiMergeColumnsSIcon } from './mergeColumns/mergeColumns.small.svg';
 export { ReactComponent as UiNestSIcon } from './nest/nest.small.svg';
 export { ReactComponent as UiOuterJoinSIcon } from './outerJoin/outerJoin.small.svg';
 export { ReactComponent as UiOutputSIcon } from './output/output.small.svg';
 export { ReactComponent as UiPivotSIcon } from './pivot/pivot.small.svg';
-export { ReactComponent as UiPostgresSourceSIcon } from './postgresSource/postgresSource.small.svg';
 export { ReactComponent as UiRecipeSIcon } from './recipe/recipe.small.svg';
 export { ReactComponent as UiRemoveDuplicateRowsSIcon } from './removeDuplicateRows/removeDuplicateRows.small.svg';
 export { ReactComponent as UiReplaceSIcon } from './replace/replace.small.svg';
-export { ReactComponent as UiReportSIcon } from './report/report.small.svg';
 export { ReactComponent as UiRightJoinSIcon } from './rightJoin/rightJoin.small.svg';
-export { ReactComponent as UiSnowflakeSourceSIcon } from './snowflakeSource/snowflakeSource.small.svg';
+export { ReactComponent as UiSearchSIcon } from './search/search.small.svg';
 export { ReactComponent as UiSortByAscendingSIcon } from './sortByAscending/sortByAscending.small.svg';
 export { ReactComponent as UiSortByDescendingSIcon } from './sortByDescending/sortByDescending.small.svg';
 export { ReactComponent as UiSourceSIcon } from './source/source.small.svg';
