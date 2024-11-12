c9a59048c85affcec3e1201275c5066a2ddc31ac
Alexey Plutalov <demiazz.py@gmail.com>
2024-11-12 12:26:18 +0300

    feat(ui-multi-selector): add support of `isWarning` and `isInvalid` properties

--- components/ui-multi-selector/src/UiMultiSelector/UiMultiSelector.css.ts
+++ components/ui-multi-selector/src/UiMultiSelector/UiMultiSelector.css.ts
@@ -1,16 +1,38 @@
-import { style } from '@vanilla-extract/css';
+import { assignVars, createThemeContract, style, styleVariants } from '@vanilla-extract/css';
 
 import { uiLayers, uiTheme } from '@tabula/ui-theme';
 
 import * as shared from '../shared.css';
 
+const border = createThemeContract({
+  default: null,
+  hover: null,
+  focus: null,
+});
+
+export const state = styleVariants({
+  isEmpty: {},
+
+  isWarning: {},
+
+  isInvalid: {},
+
+  isDisabled: {},
+});
+
 export const root = style({
   '@layer': {
     [uiLayers.components]: {
+      vars: assignVars(border, {
+        default: uiTheme.colors.borderControl.default,
+        hover: uiTheme.colors.neutralAlpha['20'],
+        focus: uiTheme.colors.neutralAlpha['40'],
+      }),
+
       position: 'relative',
 
       borderRadius: '6px',
-      border: `1px solid ${uiTheme.colors.borderControl.default}`,
+      border: `1px solid ${border.default}`,
 
       cursor: 'pointer',
 
@@ -28,34 +50,44 @@ export const root = style({
         },
 
         '&:hover': {
-          borderColor: uiTheme.colors.neutralAlpha['20'],
+          borderColor: border.hover,
         },
 
         '&:has(input:focus)': {
-          borderColor: uiTheme.colors.neutralAlpha['40'],
+          borderColor: border.focus,
           outline: `2px solid ${uiTheme.colors.borderControl.focus2}`,
         },
-      },
-    },
-  },
-});
 
-export const isEmpty = style({});
+        [`${state.isDisabled}&`]: {
+          backgroundColor: 'transparent',
 
-export const isDisabled = style({
-  '@layer': {
-    [uiLayers.components]: {
-      backgroundColor: 'transparent',
+          borderColor: border.default,
 
-      cursor: 'default',
+          cursor: 'default',
+        },
 
-      selectors: {
-        [`&:not(${isEmpty})`]: {
+        [`${state.isDisabled}&:not(${state.isEmpty})`]: {
           border: 'none',
         },
 
-        '&:hover': {
-          borderColor: uiTheme.colors.borderControl.default,
+        [`${state.isDisabled}&:hover`]: {
+          borderColor: border.default,
+        },
+
+        [`${state.isWarning}&`]: {
+          vars: assignVars(border, {
+            default: uiTheme.colors.borderControl.warning,
+            hover: uiTheme.colors.borderControl.warningFocus,
+            focus: uiTheme.colors.borderControl.warningFocus,
+          }),
+        },
+
+        [`${state.isInvalid}&`]: {
+          vars: assignVars(border, {
+            default: uiTheme.colors.borderControl.error,
+            hover: uiTheme.colors.borderControl.errorFocus,
+            focus: uiTheme.colors.borderControl.errorFocus,
+          }),
         },
       },
     },
@@ -75,7 +107,7 @@ export const chevron = style({
           right: '8px',
         },
 
-        [`${shared.sizes.small}${isEmpty} &`]: {
+        [`${shared.sizes.small}${state.isEmpty} &`]: {
           top: '8px',
           right: '12px',
         },
@@ -85,7 +117,7 @@ export const chevron = style({
           right: '8px',
         },
 
-        [`${shared.sizes.medium}${isEmpty} &`]: {
+        [`${shared.sizes.medium}${state.isEmpty} &`]: {
           top: '8px',
           right: '12px',
         },
